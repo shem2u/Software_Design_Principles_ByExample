fd45f6a8cfa7b31e2794cd9c776a0653e0eacf5e
Gregory Long <gregory.long@gmail.com>
2013-02-11 09:35:12 -0600

    Adding initial packages
    Testing to ensure nuget and .gitignore are properly configured

---
+++ src/Software_Design_Examples_Tests/Single_Responsibility/An_Initial_Design_Specification.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Software_Design_Examples_Tests.Single_Responsibility
+{
+    /// <summary>
+    /// This is an example of a class violating the Single Responsibility Principle
+    /// The tests below expose two orthoginal concerns (responsibilities)
+    /// See wiki ( http:// ) for details
+    /// </summary>
+    [TestClass]
+    public class An_Initial_Design_Specification
+    {
+    }
+}
