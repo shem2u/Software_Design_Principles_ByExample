f8ceec4a42b7b484a9ce51abc56534e75be20ed4
Gregory Long <gregory.long@gmail.com>
2013-02-12 07:50:19 -0600

    initial code complete - missing db
    basic code is complete along with tests
    missing db and code to persist results

---
+++ src/Software_Design_Examples/Single_Responsibility/An_Initial_Design/JobProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Software_Design_Examples.Single_Responsibility.An_Initial_Design
+{
+    /// <summary>
+    /// This class is responsible for scheduling engineers when given a 
+    /// valid request for work.
+    /// </summary>
+    public class JobProcessor
+    {
+        public JobRequestResult Process(JobRequest request)
+        {
+            if (request.RequestedTask != 12345)
+                return new JobRequestResult
+                    {Accepted = false, Errors = new List<IJobRequestErrors> {new UnknownJobIdError()}};
+            if (request.RequestedByDate.CompareTo(DateTime.Now) < 0)
+                return new JobRequestResult
+                    {Accepted = false, Errors = new List<IJobRequestErrors> {new JobInThePastError()}};
+            return new JobRequestResult {Accepted = true, ScheduledToBeginOn = request.RequestedByDate};
+        }
+    }
+}
---
+++ src/Software_Design_Examples/Single_Responsibility/An_Initial_Design/JobRequestResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Software_Design_Examples.Single_Responsibility.An_Initial_Design
+{
+    public class JobRequestResult
+    {
+        public bool Accepted { get; set; }
+        public IEnumerable<IJobRequestErrors> Errors { get; set; }
+
+        public DateTime ScheduledToBeginOn { get; set; }
+    }
+}
