8a8f28c390a2f0039da4f202b5a255bb611c7491
Gregory Long <gregory.long@gmail.com>
2013-02-27 18:42:55 -0600

    initial Open Closed Test classes

---
+++ src/Software_Design_Examples_Tests/Open_Closed/AddingAnotherRequestTypeSpecifications.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Software_Design_Examples_Tests.Open_Closed
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestClass]
+    public class AddingAnotherRequestTypeSpecifications : IntegratedDbTest
+    {
+    }
+}
---
+++ src/Software_Design_Examples_Tests/Open_Closed/ValidationChanges.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Software_Design_Examples_Tests.Open_Closed
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestClass]
+    public class ValidationChanges
+    {
+         
+    }
+}
